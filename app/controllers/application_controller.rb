1272c1941bb01a3d46beceb5241f251c65d1d520
Mia Genovese <miagenovese@gmail.com>
2018-02-07 21:45:00 +0000

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def home
-    render html: "hello, world!"
-  end
-  
-  def about
-  end
-  
-  def help
-  end
-
-  def contact
-  end
   
 end
