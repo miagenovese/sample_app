1586ee6e487a22747022ce838b06c356c30aaa15
Mia Genovese <miagenovese@gmail.com>
2018-02-16 01:05:57 +0000

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,7 +49,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
   
-  test "email addresses should be saved as lowercase" do
+  test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
