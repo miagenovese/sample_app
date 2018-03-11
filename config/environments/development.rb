63641b80b977d9e65a61714846ec027981bf87c3
Mia Genovese <miagenovese@gmail.com>
2018-03-11 22:05:00 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,11 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Do care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'http://sample_app-c9-miagenovese.c9users.io'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'http://sample_app-c9-miagenovese.c9users.io//rails/mailers/user_mailer/password_reset' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
