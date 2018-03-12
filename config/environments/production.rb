a4d23d5deea207871c03dd9f854a8c68dfb4ec1e
Mia Genovese <miagenovese@gmail.com>
2018-03-12 17:47:42 +0000

    Fix more bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'evening-river-42952.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app88353093@heroku.com'],
+    :password       => ENV['fbnfi5yv7485'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
