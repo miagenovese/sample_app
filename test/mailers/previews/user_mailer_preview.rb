00b2c1d8a1b5d68fc8d4143cfef98f28b4994e1d
Mia Genovese <miagenovese@gmail.com>
2018-03-13 03:22:49 +0000

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -13,7 +13,9 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at 
   # http://sample_app-c9-miagenovese.c9users.io//rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
